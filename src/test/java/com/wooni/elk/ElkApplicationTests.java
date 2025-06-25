ea783f9866cc7c3a0c51fb65199a981885c1a294
woo_ooni <heeey@nate.com>
2025-06-25 17:02:13 +0900

    first commit

---
+++ src/test/java/com/wooni/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooni.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
